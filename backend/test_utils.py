a20619026fdfe83f2915c685285012d47a051682
richardjorne <154006102+richardjorne@users.noreply.github.com>
2025-11-27 18:53:00 -0800

    update pnpm-lock and ci testing

---
+++ backend/test_utils.py
@@ -0,0 +1,10 @@
+import unittest
+from backend.utils import add
+
+class TestUtils(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(1, 1), 2)
+
+if __name__ == '__main__':
+    unittest.main()
