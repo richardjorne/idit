93b3a175e49b3709e6d00201e3c29da0bebdc55c
Zhenyu227 <zhw227@pitt.edu>
2025-11-28 16:19:13 -0500

    Fix import path

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
-from database import Base
+from backend.database import Base
 
 class User(Base):
     __tablename__ = "users"
